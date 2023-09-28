b90a3be60de06679581c18d07ed1c37867ef0cef
Guilherme <guilhermeagenor1012@gmail.com>
2023-09-28 13:16:11 -0300

    Prestadores

---
+++ DB/ConnectionContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+public class ConnectionContext : DbContext
+{
+  // O DbSet é responsável por mapear no banco de dados e retornar o mapeamento de acordo com a classe
+  public DbSet<Cliente> Cliente { get; set; }
+  public DbSet<CategoriaServico> Categoria { get; set; }
+  public DbSet<Prestadores> Prestador { get; set; }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseNpgsql(
+      "Server=localhost;" +
+      "Port=5432;" +
+      "Database=projeto_ds_visual;" +
+      "User Id=postgres;" +
+      "Password=gui_123@guilherme;"
+    );
+  }
+}
