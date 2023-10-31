6dd6423834ce1843bd92e20167c99f5484f51467
Kemuel Batista <kemuellima20@gmail.com>
2023-10-30 23:54:10 -0300

    feat: adding prestadores model, components and services

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ClientesComponent } from './components/clientes/clientes.component';
 import { AvaliacaoComponent } from './components/avaliacao/avaliacao.component';
 import { CategoriaServicosComponent } from './components/categoria-servicos/categoria-servicos.component';
 import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { PrestadoresComponent } from './components/prestadores/prestadores.component';
 
 const routes: Routes = [
   {path: 'clientes', component: ClientesComponent},
   {path: 'avaliacao', component: AvaliacaoComponent},
   {path: 'categorias', component: CategoriaServicosComponent},
   {path: 'pedidos', component: PedidosComponent},
+  {path: 'prestadores', component: PrestadoresComponent},
 ];
 
 @NgModule({
