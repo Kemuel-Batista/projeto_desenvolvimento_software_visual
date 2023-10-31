7141709befe7104fa8999f50ea993243aef629a6
Kemuel Batista <kemuellima20@gmail.com>
2023-10-30 23:50:07 -0300

    feat: adding avaliacao model, component and service

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { AvaliacaoComponent } from './components/avaliacao/avaliacao.component';
 
-const routes: Routes = [{
-  path: 'clientes', component: ClientesComponent
-}];
+const routes: Routes = [
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'avaliacao', component: AvaliacaoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
