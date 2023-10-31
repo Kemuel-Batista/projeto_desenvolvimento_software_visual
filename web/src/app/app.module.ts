e80fb10ea74e3f63947ca1eb1c92fa246dca5111
Kemuel Batista <kemuellima20@gmail.com>
2023-10-30 23:55:36 -0300

    chore: adding services and components to app.module and config api url

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -10,13 +10,33 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
-import { ClientesService } from './services/clientes.service';
-import { ClientesComponent } from './components/clientes/clientes.component'
+import { ClientesService } from './services/clientes/clientes.service';
+import { ClientesComponent } from './components/clientes/clientes.component';
+
+import { ServicosService } from './services/servicos/servicos.service'
+import { ServicosComponent } from './components/servicos/servicos.component';
+
+import { PrestadoresService } from './services/prestadores/prestadores.service';
+import { PrestadoresComponent } from './components/prestadores/prestadores.component';
+
+import { PedidosService } from './services/pedidos/pedidos.service';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+
+import { CategoriaServicosService } from './services/categoria-servicos/categoria-servicos.service';
+import { CategoriaServicosComponent } from './components/categoria-servicos/categoria-servicos.component';
+
+import { AvaliacaoService } from './services/avaliacao/avaliacao.service';
+import { AvaliacaoComponent } from './components/avaliacao/avaliacao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent
+    ClientesComponent,
+    ServicosComponent,
+    PrestadoresComponent,
+    PedidosComponent,
+    CategoriaServicosComponent,
+    AvaliacaoComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +46,7 @@ import { ClientesComponent } from './components/clientes/clientes.component'
     ReactiveFormsModule,
     ModalModule
   ],
-  providers: [HttpClientModule, ClientesService],
+  providers: [HttpClientModule, ClientesService, ServicosService, PrestadoresService, PedidosService, CategoriaServicosService, AvaliacaoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
