18086a44fac7b36e256bef93a5458b505999bbec
Kemuel Batista <kemuellima20@gmail.com>
2023-10-30 23:52:40 -0300

    feat: adding pedidos models, components and services

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './components/clientes/clientes.component';
 import { AvaliacaoComponent } from './components/avaliacao/avaliacao.component';
 import { CategoriaServicosComponent } from './components/categoria-servicos/categoria-servicos.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
 
 const routes: Routes = [
   {path: 'clientes', component: ClientesComponent},
   {path: 'avaliacao', component: AvaliacaoComponent},
   {path: 'categorias', component: CategoriaServicosComponent},
+  {path: 'pedidos', component: PedidosComponent},
 ];
 
 @NgModule({
